2a8c95eedcad2446b60773aafe9fd3ae96bde211
Guillermo Mejías <gmejias97@gmail.com>
2023-03-23 17:36:06 +0100

    add reset-password endpoint

--- src/controllers/reset-password.controller.ts
+++ src/controllers/reset-password.controller.ts
@@ -1,135 +1,121 @@
-import { inject } from '@loopback/core';
-import {
-  post,
-  Request,
-  Response,
-  RestBindings,
-} from '@loopback/rest';
+import {inject} from '@loopback/core';
+import {post, Request, Response, RestBindings} from '@loopback/rest';
 
-import axios from "axios";
-import qs from 'qs';
-const fs = require('fs');
-
-const sanitizer = require('sanitizer');
-
+import { AxiosError } from 'axios';
+import KeycloakController from '../providers/keycloak.provider';
+import Logger from '../services/logger.provider';
 
 export class ResetPasswordController {
-  constructor(@inject(RestBindings.Http.REQUEST) private req: Request,
-    @inject(RestBindings.Http.RESPONSE) private response: Response) { }
+  constructor(
+    @inject(RestBindings.Http.REQUEST) private req: Request,
+    @inject(RestBindings.Http.RESPONSE) private response: Response,
+  ) {}
 
-  readDockerSecret = function read(secretName = '') {
-    let secretPath = `/run/secrets/${secretName}`;
-    try {
-      return fs.readFileSync(secretPath, 'utf8');
-    } catch (err) {
-      if (err.code !== 'ENOENT') {
-        console.log(
-          `An error occurred while trying to read the secret: ${secretName}. Err: ${err}`,
+  keycloakController = new KeycloakController();
+
+
+  processAxiosError = (error: AxiosError, response?: any) => {
+    let statusCode = null;
+    if (error.response) {
+      // The request was made and the server responded with a status code outside of 2xx
+      let errorMessage = error.response.data.errorMessage;
+      let errorData = error.response.data.error;
+      let errorStatusCode = error.response.status;
+      let errorUrl = error.config.url;
+      try {
+        Logger.log(
+          `[Process Axios Error] Error Details: ${error.response.data.error.details}`,
         );
-      } else {
-        console.log(
-          `Could not find the secret, probably not running in swarm mode: ${secretName}. Err: ${err}`,
+      } catch (error) {}
+      if (errorUrl) Logger.log(`[Process Axios Error] Error URL: ${errorUrl}`);
+      if (errorMessage)
+        Logger.log(`[Process Axios Error] Error Message: ${errorMessage}`);
+      if (errorData)
+        Logger.log(
+          `[Process Axios Error] Error data: ${JSON.stringify(errorData)}`,
         );
+      if (errorStatusCode)
+        Logger.log(
+          `[Process Axios Error] Error status code: ${errorStatusCode}`,
+        );
+
+      let message;
+      switch (error.response.status) {
+        case 409:
+          message = 'User exists with same email';
+          break;
+        case 404:
+          message = 'Service unavailable';
+          statusCode = 503;
+          break;
+        case 422:
+          message = 'Unprocessable entity. Send correct body in petition';
+          break;
+        case 503:
+          message = 'Service unavailable';
+          break;
+        default:
+          break;
       }
-      return false;
+      if (!response) {
+        response = {};
+      }
+      response.message = message;
+      response.reason = errorMessage;
+      this.response.status(statusCode || error.response.status).send(response);
+    } else {
+      Logger.log(JSON.stringify(error));
+      this.response
+        .status(500)
+        .send(
+          this.generateErrorResponse(
+            'Internal Error registering the user.',
+            error.message,
+          ),
+        );
     }
   };
   
+  generateErrorResponse = (
+    message: any = 'There was an error creating the user.',
+    reason: any = 'unknown',
+  ) => {
+    return {
+      created: false,
+      message: message,
+      reason: reason,
+    };
+  };
 
-  keycloakPath = process.env.KEYCLOAK_BASE_PATH ?? 'https://keycloak.pandevita.d.lst.tfo.upm.es/';
-  usersEndpoint = 'users'
-
-  realm = process.env.PANDEVITA_REALM ?? 'pandevita';
-  pandevitaServiceUserFile = process.env.PANDEVITA_SERVICE_USER_FILE ?? 'pandevita-service-user-dev';
-  pandevitaServicePassFile = process.env.PANDEVITA_SERVICE_PASS_FILE ?? 'pandevita-service-pass-dev';
-  serviceUser = this.readDockerSecret(this.pandevitaServiceUserFile)
-  servicePass = this.readDockerSecret(this.pandevitaServicePassFile)
-
-  realmData = qs.stringify({
-    'client_id': 'admin-cli',
-    'grant_type': 'password',
-    'username': this.serviceUser,
-    'password': this.servicePass,
-  })
-
-  log = (msg: string) => {
-    console.log(new Date().toISOString() + msg);
-  }
-
-  get_token = async (realm = this.realm, realmData = this.realmData) => {
-    this.log("[get_token] Getting token for realm " + realm);
-    const token_response = await axios({
-      method: 'post',
-      url: this.keycloakPath + 'auth/realms/' + realm + '/protocol/openid-connect/token',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      data: realmData
-    });
-    if (token_response.status === 200) {
-      this.log("[get_token] OK");
-    } else {
-      this.log("[get_token] ERROR: NOT OK");
-      this.log(JSON.stringify(token_response))
+  authenticateService = async () => {
+    try {
+      await this.keycloakController.getToken();
+    } catch (error) {
+      Logger.log('[Get ServiceUser Token] Error');
+      this.processAxiosError(error);
+      return this.response;
     }
-    return token_response.data.access_token
-  }
+  };
 
   @post('/reset-password')
-  async create() {
+  async resetPassword() {
+    Logger.log('------------------[POST]-------------------------');
+    Logger.log('[POST] reset-password');
 
-    const email = this.req.query.email
-    if(!email || email==undefined) {
-      this.response.status(400).send("Provide email in query parameter")
-      return
+    await this.authenticateService()
+
+    const email = this.req.query.email as string;
+    if (!email || email == undefined) {
+      this.response.status(400).send('Provide email in query parameter');
+      return;
     }
 
-    let getUserId: any
     try {
-      getUserId = await axios({
-        method: 'get',
-        url: this.keycloakPath + 'auth/admin/realms/' + this.realm + '/' + this.usersEndpoint,
-        params: {
-          'email': email
-        },
-        headers: {
-          'Authorization': 'Bearer ' + await this.get_token(),
-          'Content-Type': 'application/json'
-        }
-      });
+      await this.keycloakController.resetPassword(email);
     } catch (error) {
-      console.log(error);
-      this.response.send(500).send("server error trying to reset password")
-      return
+      Logger.log('[Reset Password] Error resetting password');
+      Logger.log(JSON.stringify(error));
     }
-
-    console.log("email: " + email);
-    const matches = getUserId.data
-    let userId = null
-    for (const index in matches) {
-      if (matches[index]["email"] === email) {
-        // User exists with email providen
-        userId = matches[index]["id"]
-      }
-    }
-    if (userId === null) {
-      return "Bad address"
-    }
-
-    console.log("Reset password for userId " + userId + " with email " + email);
-    const keycloakResetPassword = await axios({
-      method: 'put',
-      url: this.keycloakPath + 'auth/admin/realms/' + this.realm + '/' + this.usersEndpoint + "/" + userId + "/execute-actions-email",
-      data: ["UPDATE_PASSWORD"],
-      headers: {
-        'Authorization': 'Bearer ' + await this.get_token(),
-        'Content-Type': 'application/json'
-      }
-    });
-    if (keycloakResetPassword.status == 204) {
-      return "OK"
-    }
-    return "Try again"
+    return this.response.status(204).send();
   }
-
 }
